6d2f7017e22e80dc20527b47009c0534708c4248
Dave Tarrant <davetaz@theodi.org>
2018-04-21 16:30:55 +0100

    First version

---
+++ js/adapt-odi-footerLink.js
@@ -0,0 +1,54 @@
+define([
+  'coreJS/adapt',
+], function(Adapt) {
+
+    var footerLink = _.extend({
+  
+    initialize: function() {
+    	this.listenTo(Adapt, 'router:course', this.updateCourse);
+    	this.listenTo(Adapt, 'pageView:ready', this.addLinks);
+        this.listenTo(Adapt, 'menuView:ready', this.addLinks);
+        this.listenTo(Adapt, 'router:page', this.updatePage);
+    },
+
+    updateCourse: function(target) {
+    	this.currentCourse = target;
+    },
+
+    getCourse: function() {
+    	return this.currentCourse;
+    },
+    
+    updatePage: function(target) {
+        this.contentObject = target;
+    },
+
+    getPage: function() {
+        return this.contentObject;
+    },
+
+    addLinks: function() {
+        try {
+            items = this.contentObject.get('_FooterLink')._items;
+        } catch(err) {}
+        if (items.length < 1) {
+            items = Adapt.course.get('_FooterLink')._items;
+        }
+        _.each(items, function(item) {
+            title = item.title;
+            link = item.link;
+            try {
+                if( $('.about-links').prop('innerHTML').trim().length > 0) {
+                    $('.about-links').append(' | ');
+                } 
+                $('.about-links').append('<a class="about" target="_blank"; href=\''+link+'\'>'+title+'</a>');
+            } catch(err) {}
+        });
+    }
+
+  }, Backbone.Events);
+  
+  footerLink.initialize();
+
+  return (footerLink);
+});
